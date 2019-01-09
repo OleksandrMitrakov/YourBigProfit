8f70c3bdd3e795597d6d148104d2f5263624a035
Your Name <you@example.com>
2019-01-09 15:52:42 +0100

    hamburgerFixed

---
+++ script.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+
+  $(document).on("scroll", function (){
+    if($(window).scrollTop() === 0)
+      $("header").removeClass("fixed");
+    else
+      $("header").attr("class", "fixed");
+  });
+
+  $(document).on("scroll", function (){
+    if($(window).width() > 500)
+      $("#menu ul").removeAttr("style");
+  });
+
+  $('#nav-icon').click(function(){
+      $(this).toggleClass('open');
+      $('#menu ul').slideToggle(500);
+    });
+
+    $('#menu li a').click(function(){
+      $('#menu ul').hide();
+      $('#nav-icon').removeClass('open');
+    });
+
+    $('#logo span').click(function(){
+      $('#menu ul').hide();
+      $('#nav-icon').removeClass('open');
+    });
+
+    $(window).resize(function(){
+      if($(window).width() > 500) {
+        $('#menu ul').removeAttr('style');
+      }
+    });
+    
+  // $(window).resize(function(){
+  //   if($(window).width() > 550) {
+  //     $('.menu li a').on("click", function() {
+  //       $('#menu ul').css("display", "block");
+  //     });
+  //   }
+  // });
+  //
+  // $(window).resize(function(){
+  //   if($(window).width() < 550) {
+  //     $('.menu li a').on("click", function() {
+  //       $('#menu ul').css("display", "none");
+  //     });
+  //   }
+  // });
+
+});
